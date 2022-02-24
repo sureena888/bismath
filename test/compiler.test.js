d17b5d8dc9aa9cc0d473ced3da902b1844e4a4ae
sureena888 <sureena888@gmail.com>
2022-02-23 17:27:06 -0800

    adding more files

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,9 +1,20 @@
 import assert from "assert"
+import { add, times } from "../src/bismath"
 
-describe("Array", () => {
-  describe("#indexOf()", () => {
-    it("should return -1 when the value is not present", () => {
-      assert.equal([1, 2, 3].indexOf(4), -1)
+describe("The Compiler", () => {
+  describe("has an add function()", () => {
+    it("should return 4 when adding 2 and 2 ", () => {
+      assert.deepEqual(add(2, 2), 4)
+    })
+  })
+  describe("has a times function", () => {
+    it("should return 4 when multiplying 2 and 2 ", () => {
+      assert.deepEqual(times(2, 2), 4)
+    })
+    it("returns 0 when multipling by 0 ", () => {
+      assert.deepEqual(times(2, 0), 0)
+      assert.deepEqual(times(0, 98765456789), 0)
+      assert.deepEqual(times(0, 0), 0)
     })
   })
 })
